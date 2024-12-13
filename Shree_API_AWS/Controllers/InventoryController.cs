c6ff4389e199de376a5ece4d792964823c40e1f1
Aadi Stark <aadistark2000@gmail.com>
2024-12-13 20:00:39 +0530

    Added Vendor and Inventory CRUD logics

--- Shree_API_AWS/Controllers/InventoryController.cs
+++ Shree_API_AWS/Controllers/InventoryController.cs
@@ -24,11 +24,30 @@ namespace Shree_API_AWS.Controllers
         }
 
         // GET: api/Employees
-        [HttpGet]
+        [HttpGet("Inventory")]
         public async Task<ActionResult<IEnumerable<InventoryList_DTO>>> GetInventoryList()
         {
             var inventory = await _context.Inventorylists.ToListAsync();
-            return Ok(_mapper.Map<IEnumerable<InventoryList_DTO>>(inventory));
+
+            var vendorDetails = await _context.Vendorlists.ToListAsync();
+
+            return Ok(new { Inventory = _mapper.Map<IEnumerable<InventoryList_DTO>>(inventory), 
+                            Vendor = _mapper.Map<IEnumerable<VendorList_DTO>>(vendorDetails)});
+        }
+
+        // GET: api/Employees
+        [HttpGet("LogInventory")]
+        public async Task<ActionResult<IEnumerable<LogInventory_DTO>>> GetLogInventoryList()
+        {
+            var logInventory = await _context.Loginventorydata.ToListAsync();
+
+            var vendorDetails = await _context.Vendorlists.ToListAsync();
+
+            var clientDetails = await _context.ClientDetails.ToListAsync();
+
+            return Ok(new { LogInventory = _mapper.Map<IEnumerable<LogInventory_DTO>>(logInventory), 
+                            Vendors = _mapper.Map<IEnumerable<VendorList_DTO>>(vendorDetails),
+                            Clients = _mapper.Map<IEnumerable<ClientDetails_DTO>>(clientDetails)});
         }
 
         [HttpPost]
@@ -61,26 +80,6 @@ namespace Shree_API_AWS.Controllers
                     _context.Entry(inventoryUpdate).State = EntityState.Modified;
                     await _context.SaveChangesAsync();
 
-                    var logInventory = new Loginventorydatum()
-                    {
-                        Typeoftransaction = inventoryData.Typeoftransaction,
-                        Dateoftransaction = DateTime.UtcNow.ToLocalTime(),
-                        Materialname = existingInventory.Materialname,
-                        Materialdescription = existingInventory.Materialdescription,
-                        Materialid = existingInventory.Materialid,
-                        Currentmaterialpriceperunit = inventoryData.Currentmaterialpriceperunit,
-                        Priceofunittransacted = inventoryData.Priceofunittransacted,
-                        Measuringunit = existingInventory.Measuringunit,
-                        Dateenteredby = inventoryData.Dateenteredby,
-                        Dateenteredon = DateTime.UtcNow.ToLocalTime(),
-                        Quantity = inventoryData.Quantity,
-                        Typeofmaterial = inventoryData.Typeofmaterial,
-                        Vendorid = inventoryData.Vendorid ?? null,
-                        Clientid = inventoryData.Clientid ?? null,
-                    };
-
-                    _context.Loginventorydata.Add(logInventory);
-                    await _context.SaveChangesAsync();
                 }
                 else
                 {
@@ -101,28 +100,29 @@ namespace Shree_API_AWS.Controllers
                     _context.Inventorylists.Add(inventoryAdd);
                     await _context.SaveChangesAsync();
 
-                    var logInventory = new Loginventorydatum()
-                    {
-                        Typeoftransaction = inventoryData.Typeoftransaction,
-                        Dateoftransaction = DateTime.UtcNow.ToLocalTime(),
-                        Materialname = inventoryData.Materialname,
-                        Materialdescription = inventoryData.Materialdescription,
-                        Materialid = inventoryData.Materialid,
-                        Currentmaterialpriceperunit = inventoryData.Currentmaterialpriceperunit,
-                        Priceofunittransacted = inventoryData.Priceofunittransacted,
-                        Measuringunit = inventoryData.Measuringunit,
-                        Dateenteredby = inventoryData.Dateenteredby,
-                        Dateenteredon = DateTime.UtcNow.ToLocalTime(),
-                        Quantity = inventoryData.Quantity,
-                        Typeofmaterial = inventoryData.Typeofmaterial,
-                        Vendorid = inventoryData.Vendorid ?? null,
-                        Clientid = inventoryData.Clientid ?? null,
-                    };
-
-                    _context.Loginventorydata.Add(logInventory);
-                    await _context.SaveChangesAsync();
-
                 }
+
+                var logInventory = new Loginventorydatum()
+                {
+                    Typeoftransaction = inventoryData.Typeoftransaction,
+                    Dateoftransaction = DateTime.UtcNow.ToLocalTime(),
+                    Materialname = inventoryData.Materialname,
+                    Materialdescription = inventoryData.Materialdescription,
+                    Materialid = inventoryData.Materialid,
+                    Currentmaterialpriceperunit = inventoryData.Currentmaterialpriceperunit,
+                    Priceofunittransacted = inventoryData.Priceofunittransacted,
+                    Measuringunit = inventoryData.Measuringunit,
+                    Dateenteredby = inventoryData.Dateenteredby,
+                    Dateenteredon = DateTime.UtcNow.ToLocalTime(),
+                    Quantity = inventoryData.Quantity,
+                    Typeofmaterial = inventoryData.Typeofmaterial,
+                    Vendorid = inventoryData.Vendorid ?? null,
+                    Clientid = inventoryData.Clientid ?? null,
+                };
+
+                _context.Loginventorydata.Add(logInventory);
+                await _context.SaveChangesAsync();
+
                 return Ok("Success");
 
             }catch(Exception ex)
---
+++ Shree_API_AWS/Controllers/VendorController.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shree_API_AWS.Context;
+using Shree_API_AWS.DataTransferObjects;
+using Shree_API_AWS.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Shree_API_AWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ShreeDbContext_Postgres _context;
+        public VendorController(IMapper mapper, ShreeDbContext_Postgres shreeDbContext)
+        {
+            _mapper = mapper;
+            _context = shreeDbContext;
+
+        }
+        // GET: api/<VendorController>
+        [HttpGet]
+        public async Task<IActionResult> GetVendorDetails()
+        {
+            var vendorDetails = await _context.Vendorlists.ToListAsync();
+            return Ok(_mapper.Map<IEnumerable<VendorList_DTO>>(vendorDetails));
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostVendorDetails(VendorList_DTO vendorData)
+        {
+            var existingVendorDetail = await _context.Vendorlists.Where(x => x.Vendorid == vendorData.Vendorid).FirstOrDefaultAsync();
+
+            try
+            {
+                if (existingVendorDetail != null)
+                {
+                    var vendorDatum = new Vendorlist
+                    {
+                        Id = existingVendorDetail.Id,
+                        Vendorid = existingVendorDetail.Vendorid,
+                        Vendorname = existingVendorDetail.Vendorname,
+                        Vendorlocation = existingVendorDetail.Vendorlocation,
+                        Vendortype = vendorData.Vendortype,
+                        Dataenteredby = "Admin",
+                        Dataenteredon = DateTime.UtcNow.ToLocalTime(),
+                        Gstnumber = vendorData.Gstnumber,
+                        Ownername = vendorData.Ownername,
+                        Ownernumber = vendorData.Ownernumber,
+                    };
+
+                    _context.Entry(vendorDatum).State = EntityState.Modified;
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    var vendorDatum = new Vendorlist
+                    {
+                        Vendorid = vendorData.Vendorid,
+                        Vendorname = vendorData.Vendorname,
+                        Vendorlocation = vendorData.Vendorlocation,
+                        Vendortype = vendorData.Vendortype,
+                        Dataenteredby = "Admin",
+                        Dataenteredon = DateTime.UtcNow.ToLocalTime(),
+                        Gstnumber = vendorData.Gstnumber,
+                        Ownername = vendorData.Ownername,
+                        Ownernumber = vendorData.Ownernumber,
+                    };
+
+                    _context.Vendorlists.Add(vendorDatum);
+                    await _context.SaveChangesAsync();
+
+                }
+
+                return Ok("Success");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex);
+            }
+            
+        }
+
+    }
+}
