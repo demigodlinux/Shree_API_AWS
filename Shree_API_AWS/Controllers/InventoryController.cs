90074822e2bff2e7d34691aa6fd0dea6d2a7b5f4
Aadi Stark <aadistark2000@gmail.com>
2024-12-16 19:35:54 +0530

    Modified inventory controller changes

--- Shree_API_AWS/Controllers/InventoryController.cs
+++ Shree_API_AWS/Controllers/InventoryController.cs
@@ -1,9 +1,6 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Shree_API_AWS.Attributes;
 using Shree_API_AWS.Context;
 using Shree_API_AWS.DataTransferObjects;
 using Shree_API_AWS.Models;
@@ -31,8 +28,12 @@ namespace Shree_API_AWS.Controllers
 
             var vendorDetails = await _context.Vendorlists.ToListAsync();
 
+            var clientDetails = await _context.ClientDetails.ToListAsync();
+
             return Ok(new { Inventory = _mapper.Map<IEnumerable<InventoryList_DTO>>(inventory), 
-                            Vendor = _mapper.Map<IEnumerable<VendorList_DTO>>(vendorDetails)});
+                            Vendor = _mapper.Map<IEnumerable<VendorList_DTO>>(vendorDetails),
+                            Client = _mapper.Map<IEnumerable<ClientDetails_DTO>>(clientDetails)
+            });
         }
 
         // GET: api/Employees
@@ -41,95 +42,90 @@ namespace Shree_API_AWS.Controllers
         {
             var logInventory = await _context.Loginventorydata.ToListAsync();
 
-            var vendorDetails = await _context.Vendorlists.ToListAsync();
-
-            var clientDetails = await _context.ClientDetails.ToListAsync();
-
-            return Ok(new { LogInventory = _mapper.Map<IEnumerable<LogInventory_DTO>>(logInventory), 
-                            Vendors = _mapper.Map<IEnumerable<VendorList_DTO>>(vendorDetails),
-                            Clients = _mapper.Map<IEnumerable<ClientDetails_DTO>>(clientDetails)});
+            return Ok(_mapper.Map<IEnumerable<LogInventory_DTO>>(logInventory));
         }
 
         [HttpPost]
-        public async Task<ActionResult<IEnumerable<string>>> PostInventoryData(LogInventory_DTO inventoryData)
+        public async Task<ActionResult<string>> PostInventoryData(List<LogInventory_DTO> inventoryTransactions)
         {
-            var existingInventory = await _context.Inventorylists
-                .Where(x => x.Materialname == inventoryData.Materialname && 
+            foreach(var inventoryData in inventoryTransactions)
+            {
+                var existingInventory = await _context.Inventorylists
+                .Where(x => x.Materialname == inventoryData.Materialname &&
                     (inventoryData.Typeoftransaction.Equals("Inward") || inventoryData.Typeoftransaction.Equals("Outwards")))
                 .FirstOrDefaultAsync();
-            try
-            {
-                if (existingInventory != null)
+                try
                 {
-                    var inventoryUpdate = new Inventorylist()
+                    if (existingInventory != null)
                     {
-                        Materialname = existingInventory.Materialname,
-                        Materialdescription = existingInventory.Materialdescription,
-                        Materialid = existingInventory.Materialid,
-                        Materialpriceperunit = (existingInventory.Materialpriceperunit + inventoryData.Currentmaterialpriceperunit) / 2,
-                        Measuringunit = existingInventory.Measuringunit,
-                        Dateenteredby = inventoryData.Dateenteredby,
-                        Dateenteredon = DateTime.UtcNow.ToLocalTime(),
-                        Id = existingInventory.Id,
-                        Quantity = inventoryData.Typeoftransaction.Equals("Inward") ?
-                            existingInventory.Quantity + inventoryData.Quantity : existingInventory.Quantity - inventoryData.Quantity,
-                        Typeofmaterial = inventoryData.Typeofmaterial,
-                        Vendorid = inventoryData.Vendorid ?? null,
-                    };
+                        // Update the existing inventory
+                        existingInventory.Materialpriceperunit =
+                            (existingInventory.Materialpriceperunit + inventoryData.Currentmaterialpriceperunit) / 2;
 
-                    _context.Entry(inventoryUpdate).State = EntityState.Modified;
-                    await _context.SaveChangesAsync();
+                        existingInventory.Dateenteredby = inventoryData.Dateenteredby;
+                        existingInventory.Dateenteredon = DateTime.UtcNow.ToLocalTime();
 
-                }
-                else
-                {
-                    var inventoryAdd = new Inventorylist()
+                        existingInventory.Quantity = inventoryData.Typeoftransaction.Equals("Inward") ?
+                        existingInventory.Quantity + inventoryData.Quantity : existingInventory.Quantity - inventoryData.Quantity;
+
+                        existingInventory.Typeofmaterial = inventoryData.Typeofmaterial;
+                        existingInventory.Vendorid = inventoryData.Vendorid ?? null;
+
+                        // Save changes for the existing entity
+                        await _context.SaveChangesAsync();
+                    }
+                    else
                     {
+                        // Add new inventory if it doesn't exist
+                        var inventoryAdd = new Inventorylist()
+                        {
+                            Materialname = inventoryData.Materialname,
+                            Materialdescription = inventoryData.Materialdescription,
+                            Materialid = inventoryData.Materialid,
+                            Materialpriceperunit = inventoryData.Currentmaterialpriceperunit,
+                            Measuringunit = inventoryData.Measuringunit,
+                            Dateenteredby = inventoryData.Dateenteredby,
+                            Dateenteredon = DateTime.UtcNow.ToLocalTime(),
+                            Quantity = inventoryData.Quantity,
+                            Typeofmaterial = inventoryData.Typeofmaterial,
+                            Vendorid = inventoryData.Vendorid ?? null,
+                        };
+
+                        _context.Inventorylists.Add(inventoryAdd);
+                        await _context.SaveChangesAsync();
+                    }
+
+                    // Add a log entry
+                    var logInventory = new Loginventorydatum()
+                    {
+                        Typeoftransaction = inventoryData.Typeoftransaction,
+                        Dateoftransaction = DateTime.UtcNow.ToLocalTime(),
                         Materialname = inventoryData.Materialname,
                         Materialdescription = inventoryData.Materialdescription,
                         Materialid = inventoryData.Materialid,
-                        Materialpriceperunit = inventoryData.Currentmaterialpriceperunit,
+                        Currentmaterialpriceperunit = inventoryData.Currentmaterialpriceperunit,
+                        Priceofunittransacted = inventoryData.Priceofunittransacted,
                         Measuringunit = inventoryData.Measuringunit,
                         Dateenteredby = inventoryData.Dateenteredby,
                         Dateenteredon = DateTime.UtcNow.ToLocalTime(),
                         Quantity = inventoryData.Quantity,
                         Typeofmaterial = inventoryData.Typeofmaterial,
                         Vendorid = inventoryData.Vendorid ?? null,
+                        Clientid = inventoryData.Clientid ?? null,
                     };
 
-                    _context.Inventorylists.Add(inventoryAdd);
+                    _context.Loginventorydata.Add(logInventory);
                     await _context.SaveChangesAsync();
 
+                    return Ok("Success");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
                 }
 
-                var logInventory = new Loginventorydatum()
-                {
-                    Typeoftransaction = inventoryData.Typeoftransaction,
-                    Dateoftransaction = DateTime.UtcNow.ToLocalTime(),
-                    Materialname = inventoryData.Materialname,
-                    Materialdescription = inventoryData.Materialdescription,
-                    Materialid = inventoryData.Materialid,
-                    Currentmaterialpriceperunit = inventoryData.Currentmaterialpriceperunit,
-                    Priceofunittransacted = inventoryData.Priceofunittransacted,
-                    Measuringunit = inventoryData.Measuringunit,
-                    Dateenteredby = inventoryData.Dateenteredby,
-                    Dateenteredon = DateTime.UtcNow.ToLocalTime(),
-                    Quantity = inventoryData.Quantity,
-                    Typeofmaterial = inventoryData.Typeofmaterial,
-                    Vendorid = inventoryData.Vendorid ?? null,
-                    Clientid = inventoryData.Clientid ?? null,
-                };
-
-                _context.Loginventorydata.Add(logInventory);
-                await _context.SaveChangesAsync();
-
-                return Ok("Success");
-
-            }catch(Exception ex)
-            {
-                return BadRequest(ex.Message);
             }
-            
+            return Ok("Success");
         }
 
     }
